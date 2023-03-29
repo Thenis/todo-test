d296920a7b4bad2c1e2db7f8d867d5ee149f2ebb
Denis Stoilov <d.stoilov96@gmail.com>
2023-03-29 16:24:40 +0300

    feat: add todo repo

--- src/infrastructure/repositories/todo.repository.ts
+++ src/infrastructure/repositories/todo.repository.ts
@@ -3,17 +3,21 @@ import { singleton } from "tsyringe";
 import { ITodoRepository } from "../interfaces/todo-repository.interface";
 import { TodoModel } from "../models/todo.model";
 
+const fakeHttpCall = (url: string) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        content: "Some content",
+        completed: false,
+      });
+    }, 1500);
+  });
+};
+
 @singleton()
 export class TodoRepository implements ITodoRepository {
   @TrackRequest()
-  get(): Promise<TodoModel> {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          content: "Some content",
-          completed: false,
-        });
-      }, 1500);
-    });
+  async get(): Promise<TodoModel> {
+    return (await fakeHttpCall("my-url")) as Promise<TodoModel>;
   }
 }
